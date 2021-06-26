96e30afa4604e68a9bc0fb03e8ae4d48797c3bf0
honasgame <65813992+honasgame@users.noreply.github.com>
2021-06-26 10:22:37 +0000

    added new code

--- main.py
+++ main.py
@@ -10,12 +10,24 @@ def get_quote():
   response = requests.get(url).json()
   return response
 
+def get_quote_today():
+  url = 'https://zenquotes.io/api/today'
+  response_2 = requests.get(url).json()
+  return response_2
+
 def build_quote():
   api_response = get_quote()
   quote = api_response[0]['q']
   author = api_response[0]['a']
   complete_quote = quote + ' -' + author
   return complete_quote
+  
+def build_quote_today():
+  api_response_2 = get_quote_today()
+  quote_2 = api_response_2[0]['q']
+  author_2 = api_response_2[0]['a']
+  complete_quote_2 = quote_2 + ' -' + author_2
+  return complete_quote_2
 
 @client.event
 async def on_ready():
@@ -30,8 +42,9 @@ async def on_message(message):
     "$hello": "Hello, this is the wild bot",
     "$howareyou": "I am fine thank you",
     "$favgames": "My three favourite games are cricket, football and badminton",
-    "$help": "Available commands: [$hello, $howareyou, $favgames, $inspire_me]",
-    "$inspire_me": build_quote()
+    "$help": "Available commands: [$hello: conversation, $howareyou: conversation, $favgames : conversation, $inspire_me: random inspirational quotes,$inspire_me_today: inspirational quote of the today]",
+    "$inspire_me": build_quote(),
+    "$inspire_me_today":build_quote_today()
   }
 
   if msg in request_response:
